af9ae596ce017945cdb1300cde13f17f7af8b4da
魏小伟 <1550518923@qq.com>
2020-07-26 01:28:17 +0800

    gateway 网关源码 学习

---
+++ cloud-gateway-server9527/src/main/java/com/wxw/cloud/study/SourceStudy.java
@@ -0,0 +1,13 @@
+package com.wxw.cloud.study;
+
+import org.springframework.cloud.gateway.config.GatewayAutoConfiguration;
+
+/**
+ * @Author: wxw
+ * @create: 2020-07-26-0:39
+ */
+public class SourceStudy {
+    public static void main(String[] args) {
+        GatewayAutoConfiguration gatewayAutoConfiguration = new GatewayAutoConfiguration();
+    }
+}
